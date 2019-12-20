109ff3a114cdf17874a75cefa877d3a42f023a64
SkirdinNikita <skirdin.nik@gmail.com>
2019-12-20 19:38:36 +0300

    added profile info class

---
+++ src/scripts/Components/Profile/ProfileInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class ProfileInfo extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <div className="header-main__profile-info">Это информация профиля</div>
+    }
+}
+export default ProfileInfo
