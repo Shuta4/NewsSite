9d2ce6d27bc43bffd966c2954d34fa68c6bae884
SkirdinNikita <skirdin.nik@gmail.com>
2019-12-20 19:38:08 +0300

    added navigation class

---
+++ src/scripts/Components/Navigation/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Navigation extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return <div className="navigation-main">
+            Это навигация
+        </div>
+    }
+}
+export default Navigation
