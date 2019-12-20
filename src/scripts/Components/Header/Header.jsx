9c5d3a9f46965e4f3524bbe8e8289506db240107
SkirdinNikita <skirdin.nik@gmail.com>
2019-12-20 19:37:34 +0300

    Update header, added start structure and logo div

--- src/scripts/Components/Header/Header.jsx
+++ src/scripts/Components/Header/Header.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Navigation from "../Navigation/Navigation.jsx"
+import ProfileInfo from "../Profile/ProfileInfo.jsx"
 
 class Header extends React.Component {
     constructor(props) {
@@ -6,7 +8,14 @@ class Header extends React.Component {
     }
 
     render() {
-        return <header className="header-main">Это хедер</header>
+        return <header className="header-main">
+                <div className="header-main__logo">
+                    <img src="https://www.101domain.ru/images/flags/large/NEWS.png" alt="logo" className="header-main__logo__image" />
+                    <h1 className="header-main__logo__text">News site</h1>
+                </div>
+                <ProfileInfo />
+                <Navigation />
+            </header>
     }
 }
 export default Header
