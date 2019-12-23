219804968d6240830cc87c7de37cbff0393ee288
SkirdinNikita <skirdin.nik@gmail.com>
2019-12-23 19:33:25 +0300

    Rename main.jsx to App.jsx

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom';
 import React from "react";
 import Page from "./Components/Page/Page.jsx";
+import "../style.css"
 
 const contArr = [{
     contactName: "Телефон",
