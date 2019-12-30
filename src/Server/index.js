03f46c245654de0e4de1f66e1dcdf01164eb1d03
SkirdinNikita <skirdin.nik@gmail.com>
2019-12-30 20:41:43 +0300

    server update

--- src/Server/index.js
+++ src/Server/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+const express = require('express');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const port = 3000;
 const server = express();
+mongoose.connect("mongodb://localhost/aticles-db", { useNewUrlParser: true, useUnifiedTopology: true });
 
 server.listen(port)
 console.log("Server are running on port: " + port);
 
-server.use('/api', require('./api.js'))
+server.use('/api', require('./api.js'));
+server.use(bodyParser.json());
