dab478e04e9b1380d76531b8910538b1aa8f7a14
SkirdinNikita <skirdin.nik@gmail.com>
2019-12-30 15:32:25 +0300

    make start server

--- src/Server/index.js
+++ src/Server/index.js
@@ -3,23 +3,7 @@ var express = require('express');
 const port = 3000;
 const server = express();
 
-server.get('/', (req, res) => {
-    res.send("Its home page.")
-});
-
-server.get("/about", (req, res) => {
-    res.send('Its about page.')
-});
-
-server.get('/articles/:id', (req, res) => {
-    res.send('Its article with id ' + req.params.id + ".")
-});
-
-
-//Последняя проверка для выведения ошибки 404 page not found
-server.get('/*', (req, res) => {
-    res.send("Error 404: page " + req.url + " not found.")
-});
-
 server.listen(port)
 console.log("Server are running on port: " + port);
+
+server.use('/api', require('./api.js'))
