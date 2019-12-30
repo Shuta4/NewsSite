fff62dcf47a02a9f3fa20c06478971eca40ecbdd
SkirdinNikita <skirdin.nik@gmail.com>
2019-12-30 20:40:55 +0300

    add schemas

---
+++ src/Server/db/Schemas/article.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ArticleSchema = new Schema({
+    id: String,
+    header: String,
+    text: String,
+    sourceLink: String,
+    sourceName: String,
+    creationDateText: String, 
+    creationDate: Date
+});
+
+const Article = mongoose.model("article", ArticleSchema);
+module.exports = Article;
