6ea46cee0c9fb8cd55dcfc37d94cb439eb20a30d
SkirdinNikita <skirdin.nik@gmail.com>
2019-12-30 20:41:23 +0300

    update api

--- src/Server/api.js
+++ src/Server/api.js
@@ -1,36 +1,44 @@
 const express = require('express');
 const router = express.Router();
-
-router.get('/articles', (req, res) => {
-    // All articles
-})
-
-router.get('/articles/name/:name', (req, res) => {
-    // Article by its name
-})
-
-router.get('/articles/id/:id', (req, res)=> {
-    // Article by id
-})
+const Article = require('./db/Schemas/article')
 
 router.get('/start', (req, res) => {
-    // siteImage, siteName, route and navigation Arrs 
-})
-
-router.delete('/articles/:id', (req, res) => {
-    // delete article with "id" == "req.params.id"
-})
-
-router.post('/articles', (req, res) => {
-    // Add article
-})
-
-router.put('/articles/:id', (req, res) => {
-    // Update article with "id" == "req.params.id"
-})
+    // siteImage, siteName, route, contacts and navigation Arrs
+});
 
 router.put('/start', (req, res) => {
     // Update site information
-})
+});
+
+router.get('/articles', (req, res) => {
+    // All articles
+    Article.find({}).then(articles => res.send(articles))
+});
+
+router.get('/articles/id/:id', (req, res)=> {
+    // Article by id
+    Article.findOne({id: req.params.id}).then(article => res.send(article))
+});
+
+router.get('/articles/header/:header', (req, res) => {
+    // Article by its header
+    Article.find({header: req.params.header}).then(article => res.send(article))
+});
+
+router.delete('/articles/:id', (req, res) => {
+    // delete article with "id" == "req.params.id"
+    Article.deleteOne({id: req.params.id})
+});
+
+router.post('/articles', (req, res) => {
+    // Add article
+    Article.create(req.body).then(article => res.send(article))
+});
+
+router.put('/articles/:id', (req, res) => {
+    // Update article with "id" == "req.params.id"
+    Article.findOneAndUpdate({id: req.params.id}, req.body).then(() => Article.findOne({id: req.params.id}))
+        .then(article => res.send(article))
+});
 
 module.exports = router;
