d0e5cb57e6298712660dd1f07984bf0d84251a29
SkirdinNikita <skirdin.nik@gmail.com>
2020-01-01 13:27:56 +0300

    added form for finding articles

---
+++ src/App/Components/FindArticle.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+class FindArticle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handlerSubmit = this.handlerSubmit.bind(this);
+    }
+    handlerSubmit(event) {
+        event.preventDefault();
+        var form = event.target;
+        
+    }
+    render() {
+        return <form className="article-find" onSubmit={this.handlerSubmit}>
+            <select name="select"> 
+                <option value="name">Искать по имени</option>
+                <option value="id">Искать по ID</option>
+            </select>
+            <input type="text" />
+            <input type="submit" />
+        </form>
+    }
+}
+
+export default FindArticle;
