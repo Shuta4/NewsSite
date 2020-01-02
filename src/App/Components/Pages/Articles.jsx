b5ac1033416f128648de9c92c4795968230d26f0
SkirdinNikita <skirdin.nik@gmail.com>
2020-01-02 21:06:44 +0300

    articles page update

--- src/App/Components/Pages/Articles.jsx
+++ src/App/Components/Pages/Articles.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import FindArticle from '../FindArticle.jsx';
 import AddArticle from '../AddArticle.jsx'
+import AllArticles from '../AllArticles.jsx'
 
 class Articles extends React.Component {
     constructor(props) {
         super(props);
     }
-    componentDidMount() {
-        document.querySelector('.articles').querySelector(".add-article-button").addEventListener("click", () => {})
-    }
     render() {
         return <div className="articles">
-            <h3>Поиск по статьям</h3>
-                <FindArticle />
-            <h3>Лучшие статьи</h3>
-            <button className="add-article-button">Добавить статью</button>
+            <FindArticle />
+            <AllArticles />
             <AddArticle />
         </div>
     }
