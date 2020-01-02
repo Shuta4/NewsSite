723c17ad16fd529cf669bf1d6bafff1afe127c60
SkirdinNikita <skirdin.nik@gmail.com>
2020-01-02 21:05:10 +0300

    Server fix

--- src/Server/index.js
+++ src/Server/index.js
@@ -6,8 +6,8 @@ const port = 3000;
 const server = express();
 mongoose.connect("mongodb://localhost/aticles-db", { useNewUrlParser: true, useUnifiedTopology: true });
 
+server.use(bodyParser.json());
+server.use('/api', require('./api.js'));
+
 server.listen(port)
 console.log("Server are running on port: " + port);
-
-server.use('/api', require('./api.js'));
-server.use(bodyParser.json());
