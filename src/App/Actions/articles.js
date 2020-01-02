dfe30f803476e00e6646647360bb3d92579534f3
SkirdinNikita <skirdin.nik@gmail.com>
2020-01-02 21:05:47 +0300

    put all articles actions in one file

---
+++ src/App/Actions/articles.js
@@ -0,0 +1,19 @@
+export function articlesFetchDataSuccess(articles) {
+    return {
+        type: "ARTICLES_FETCH_DATA_SUCCESS",
+        articles
+    }
+}
+
+export function articlesFetchData(url) {
+    return (dispatch)=> {
+        fetch(url).then(response => {
+            if(!response.ok) throw new Error(response.statusText);
+            return response;
+        }).then(response => {
+            response.json()
+        }).then(articles => {
+            dispatch(articlesFetchDataSuccess(articles))
+        })
+    }
+}
