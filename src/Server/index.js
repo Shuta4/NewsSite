eec92cd74696ddb4b88838b3eefe4108fbbff73d
SkirdinNikita <skirdin.nik@gmail.com>
2020-01-04 13:00:31 +0300

    add headers to solve problem with cors

--- src/Server/index.js
+++ src/Server/index.js
@@ -7,6 +7,11 @@ const server = express();
 mongoose.connect("mongodb://localhost/aticles-db", { useNewUrlParser: true, useUnifiedTopology: true });
 
 server.use(bodyParser.json());
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 server.use('/api', require('./api.js'));
 
 server.listen(port)
