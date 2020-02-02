58df887a63201adaac8c67631231c22666843145
Student <Student@rubiconepro.ru>
2020-02-02 10:14:49 +0300

    added header to pass CORS policy

--- src/Server/index.js
+++ src/Server/index.js
@@ -8,8 +8,7 @@ mongoose.connect("mongodb://localhost/aticles-db", { useNewUrlParser: true, useU
 
 server.use(bodyParser.json());
 server.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*");
     next();
   });
 server.use('/api', require('./api.js'));
